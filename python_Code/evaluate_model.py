3b9d090fa1589c9c57e446cfb9e7f0a8029a25fd
ManjinderUNCC <77996106+ManjinderUNCC@users.noreply.github.com>
2024-05-03 19:37:31 -0400

    Update evaluate_model.py

--- python_Code/evaluate_model.py
+++ python_Code/evaluate_model.py
@@ -1,10 +1,9 @@
-import jsonlines
 import spacy
+import jsonlines
 from sklearn.metrics import classification_report, accuracy_score, f1_score, precision_score, recall_score
 
 # Load the trained spaCy model
-model_path = "./my_trained_model"
-nlp = spacy.load(model_path)
+nlp = spacy.load("./my_trained_model")
 
 # Load the golden evaluation data
 golden_eval_data = []
@@ -12,24 +11,19 @@ with jsonlines.open("data/goldenEval.jsonl") as reader:
     for record in reader:
         golden_eval_data.append(record)
 
-# New threshold for considering a label
-new_threshold = 0.21  # Change this to your desired threshold value
-
-# Predict labels for each record using your model with the new threshold
+# Predict labels for each record using your model
 predicted_labels = []
 for record in golden_eval_data:
     text = record["text"]
     doc = nlp(text)
-    # Apply the new threshold to the predicted labels
-    filtered_labels = {label: score for label, score in doc.cats.items() if score > new_threshold}
-    predicted_labels.append(filtered_labels)
+    predicted_labels.append(doc.cats)
 
 # Extract ground truth labels from the golden evaluation data
 true_labels = [record["accept"] for record in golden_eval_data]
 
 # Convert label format to match sklearn's classification report format
 true_labels_flat = [label[0] if label else "reject" for label in true_labels]
-predicted_labels_flat = [max(pred, key=pred.get) if pred else "reject" for pred in predicted_labels]
+predicted_labels_flat = [max(pred, key=pred.get) for pred in predicted_labels]
 
 # Calculate evaluation metrics
 accuracy = accuracy_score(true_labels_flat, predicted_labels_flat)
